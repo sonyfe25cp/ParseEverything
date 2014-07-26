738cb33a8f76663fae1cf8c24d21aa0c8a86bfc6
Omar@Techwolf <sonyfe25cp@gmail.com>
2014-07-26 14:52:40 +0800

    remove wrong folder name

--- src/main/java/com/parseeverything/sample/ParsePages.java
+++ src/main/java/com/parseeverything/sample/ParsePages.java
@@ -1,6 +1,9 @@
 package com.parseeverthing.sample;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.slf4j.Logger;
@@ -17,18 +20,24 @@ import com.parseeverthing.pages.StoredPage;
 public class ParsePages {
     static Logger logger = LoggerFactory.getLogger(ParsePages.class);
 
-    public static void main(String[] args) {
-        String filePath = "";//文件绝对路径
+    public static void main(String[] args) throws IOException {
+        String filePath = "/Users/omar/data/zhilian/zhaopin_zunyi_2014-01-29-18-01-22.pages";//文件绝对路径
         File file = new File(filePath);
         PageStoreReader psr = new PageStoreReader(file);
         ArrayList<StoredPage> list = psr.loadAll();
+        int count = 33;
         for (StoredPage page : list) {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(new File("/tmp/data_for_uts/jobs/"+count+".txt"))); 
             String url = page.getHeader("URL");
             String html = page.getContent();
             
             logger.info("url: {}", url);
-            logger.info("html: {}", html);
+//            logger.info("html: {}", html);
+            count ++;
+            bw.write(html);
+            bw.close();
         }
+        logger.info("size : {}", list.size());
         psr.close();
     }
 }
