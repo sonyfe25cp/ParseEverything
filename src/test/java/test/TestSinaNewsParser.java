e49d00b0ad7799c76e5e57a4f52bd1d1fbe37fa4
Omar@Techwolf <sonyfe25cp@gmail.com>
2014-08-12 19:25:34 +0800

    add test sina

--- src/test/java/test/TestSinaNewsParser.java
+++ src/test/java/test/TestSinaNewsParser.java
@@ -22,5 +22,15 @@ public class TestSinaNewsParser extends TestCase{
         assertEquals("2008-09-30", sina.getPublishDate());
         System.out.println(sina);
     }
+    @Test
+    public void testChanjing2(){
+        String html = Utils.getResouce("sina/sina_company_2.html");
+        SinaNewsParser snp = new SinaNewsParser();
+        String url = "http://finance.sina.com.cn/roll/20080930/21265353746.shtml";
+        NewsModel sina = snp.parse(url, html);
+        assertEquals("金百利菠萝啤是碳酸饮料 被指标识不清误导消费", sina.getTitle());
+        assertEquals("2009-07-16", sina.getPublishDate());
+        System.out.println(sina);
+    }
     
 }
